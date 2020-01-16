c905a265d5c02fc7aff8cd6005321e6f02d01f84
zhangqing <heyzqt@qq.com>
2020-01-16 17:03:13 +0800

    购买商品页从服务器获取数据

--- src/components/goods/GoodList.vue
+++ src/components/goods/GoodList.vue
@@ -1,42 +1,46 @@
 <template>
     <div>
-        <div class="goods-list">
-            <div class="goods-item">
-                <img src="https://img1.360buyimg.com/n6/jfs/t2656/295/34058120/362134/d92995e5/56fc835dNe349b797.jpg" alt="">
-                <h1 class="title">佳能（Canon）EOS 800D 单反相机 单反机身 单反套机 （EF-S 18-55mm f/4-5.6 IS STM 单反镜头）</h1>
+        <div class="goods-list" v-if="goods.length > 0">
+            <div class="goods-item" v-for="(item, index) in goods" :key="index">
+                <img :src="item.url" alt="">
+                <h1 class="title">{{item.title}}</h1>
                 <div class="price">
-                    <div><span class="now">￥899</span><span class="old">￥999</span></div>
+                    <div><span class="now">￥{{item.now_price}}</span><span class="old">￥{{item.ori_price}}</span></div>
                     <div class="sale-box">
                         <span>热卖中</span>
-                        <span>剩60件</span>
-                    </div>
-                </div>
-            </div>
-                        <div class="goods-item">
-                <img src="https://img1.360buyimg.com/n6/jfs/t1/86044/25/10192/174904/5e16c340E3902b7a9/cc266dcb26f5c317.jpg" alt="">
-                <h1 class="title">佳能（Canon） 买就送礼包佳能EOS 80D单反相机数码相机全新正品国行佳能80D套机 腾龙18-400mm 防抖长焦镜头</h1>
-                <div class="price">
-                    <div><span class="now">￥899</span><span class="old">￥999</span></div>
-                    <div class="sale-box">
-                        <span>热卖中</span>
-                        <span>剩10件</span>
-                    </div>
-                </div>
-            </div>
-                        <div class="goods-item">
-                <img src="https://img1.360buyimg.com/n6/jfs/t2656/295/34058120/362134/d92995e5/56fc835dNe349b797.jpg" alt="">
-                <h1 class="title">佳能（Canon）EOS 800D 单反相机 单反机身 单反套机 （EF-S 18-55mm f/4-5.6 IS STM 单反镜头）</h1>
-                <div class="price">
-                    <div><span class="now">￥899</span><span class="old">￥999</span></div>
-                    <div class="sale-box">
-                        <span>热卖中</span>
-                        <span>剩60件</span>
+                        <span>剩{{item.remain}}件</span>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            goods: []
+        }
+    },
+    created() {
+        this.getData();
+    },
+    methods: {
+        getData() {
+            this.axios
+                .get('./src/static/goods.json')
+                .then(resp => {
+                    if (resp.status == 200) {
+                        console.log(resp);
+                        this.goods = resp.data.goods;
+                    } else {
+                        alert('加载数据失败');
+                    }
+                })
+        }
+    }    
+}
+</script>
 <style lang="scss">
 .goods-list {
     padding: 0px 4px;
